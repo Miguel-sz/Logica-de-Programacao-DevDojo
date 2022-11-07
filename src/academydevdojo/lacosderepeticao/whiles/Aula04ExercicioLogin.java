25c4af78b016aad455f4926e21c4fecb121ecd24
Miguel-sz <miguel.dev7@gmail.com>
2022-11-07 15:43:13 -0200

    Commit

--- src/academydevdojo/lacosderepeticao/whiles/Aula04ExercicioLogin.java
+++ src/academydevdojo/lacosderepeticao/whiles/Aula04ExercicioLogin.java
@@ -12,15 +12,24 @@ public class Aula04ExercicioLogin {
     public static void main(String[] args) {
         final String login = "Luffy";
         final String password = "ReiDosPiratas";
-        boolean exibirTelaLogin = true;
+
         Scanner scanner = new Scanner(System.in);
-        while (exibirTelaLogin) {
+        while (true) {
             System.out.println("Digite o seu usuário");
             String loginDigitado = scanner.nextLine();
             System.out.println("Digite sua senha");
             String passwordDigitado = scanner.nextLine();
+            if (login.equals (loginDigitado) &&  password.equals (passwordDigitado)) {
+                System.out.println("ACESSO CONCEDIDO");
+
+                break;
+            }
+                System.out.println("ACESSO NEGADO");
+
+            }
+        System.out.println("PROGRAMA TERMINADO");
         }
 
     }
-}
+
 
